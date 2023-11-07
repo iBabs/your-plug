539d3137eb3716b19460004a63489dc83a4353f3
iBabs <ibrahimbabalola8@gmail.com>
2023-11-07 10:58:17 +0100

    while loop

--- anything.js
+++ anything.js
@@ -1,62 +1,72 @@
-let income = 2000
+// let income = 2000
 
-let expence = 800
+// let expence = 800
 
-const saving = income / 3
+// const saving = income / 3
 
-const flex = income - (expence + saving)
+// const flex = income - (expence + saving)
 
-console.log(flex)
+// console.log(flex)
 
-let sideHustle = 750
-sideHustle += 5
+// let sideHustle = 750
+// sideHustle += 5
 
-income += sideHustle
+// income += sideHustle
 
-console.log(income)
-
-
-let balance = 3000
-
-const transact = parseInt(prompt('Welcome YourPlug Banking Services\n1. Check Balance\n2. Recharge\n3.Transfer\n4.Paybills\n5.Get loan\n6. Open New Account'))
-
-//Recharge function
-function recharge() {
-    const tranType = parseInt(prompt('What type of recharge do you want\n1.data\n2.airtime'))
-
-    switch (tranType) {
-        case 1:
-            alert('Data service not available')
-            break
-        case 2:
-            const amount = parseInt(prompt('Enter Amount:'))
-            const num = prompt('Enter destination number')
-            if (amount <= balance) {
-                balance -= amount
-                alert(`Your recharge of ${num} is successful\n your new balance is ${balance}`)
-            } else {
-                alert('abeg go hustle')
-            }
-            break
-        default:
-            alert('Invalid number')
-    }
-}
+// console.log(income)
 
 
+// let balance = 3000
+
+// const transact = parseInt(prompt('Welcome YourPlug Banking Services\n1. Check Balance\n2. Recharge\n3.Transfer\n4.Paybills\n5.Get loan\n6. Open New Account'))
+
+// //Recharge function
+// function recharge() {
+//     const tranType = parseInt(prompt('What type of recharge do you want\n1.data\n2.airtime'))
+
+//     switch (tranType) {
+//         case 1:
+//             alert('Data service not available')
+//             break
+//         case 2:
+//             const amount = parseInt(prompt('Enter Amount:'))
+//             const num = prompt('Enter destination number')
+//             if (amount <= balance) {
+//                 balance -= amount
+//                 alert(`Your recharge of ${num} is successful\n your new balance is ${balance}`)
+//             } else {
+//                 alert('abeg go hustle')
+//             }
+//             break
+//         default:
+//             alert('Invalid number')
+//     }
+// }
 
 
 
-switch (transact) {
-    case 1:
-        alert(`Your balance is currently ${balance}USD`)
-        break
-    case 2:
-        recharge()
-        break
-    case 3:
-        alert('we no dey do transfer')
-        break
-    default:
-        alert('invalid option')
-}  
+
+
+// switch (transact) {
+//     case 1:
+//         alert(`Your balance is currently ${balance}USD`)
+//         break
+//     case 2:
+//         recharge()
+//         break
+//     case 3:
+//         alert('we no dey do transfer')
+//         break
+//     default:
+//         alert('invalid option')
+// }  
+
+
+// let i = 0
+// const tur = [56,65, 98, 102, 52, 54,32]
+
+// while(i< tur.length){
+//     console.log(tur[i]*2)
+//     i++
+// }
+
